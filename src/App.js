b508092243dd28d46f6c0a4139cb6bb1d36f205d
usman rahim <usmanrahim2000@gmail.com>
2022-11-07 19:20:56 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render(){
+    let sub = "Software Engineering";
+
+    let frstName = "Usman"
+    let lstName = "Rahim";
+
+    let arr = [1,2,3,4,5]
+    return(
+      <div>
+        <h1>Hello, I am Usman Rahim</h1>
+        <p>I am a Software Engineer</p>
+        <br></br>
+        {/* template literals or react likhny mn dono mn curly brackets lagatay pr dollar nahi */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am doing {sub} from KU.
+          {`my Name is ${frstName} ${lstName}`}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h4>{arr}</h4>
+      </div>      
+    )
+  }
 }
 
 export default App;
